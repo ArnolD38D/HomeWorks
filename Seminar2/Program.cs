2dbe066a7dd2b2dc940910c999a7e8dc40e0dfe5
Arnold <usov.a38@mail.ru>
2022-08-14 20:57:59 +0800

    SecondSeminarCommit2

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -16,39 +16,57 @@ int Digit = SecondNumberOfThree(number);
 Console.WriteLine ($"{number} the second number {Digit}");
 */
 
-
+/*
 // Задача 13. Напишите программу, которая выводит третью цифру 
 // заданного числа или сообщает, что третьей цифры нет.
 
+
 int MethodThirdNumber (int number)
 {
-    while (number >= 1000)
+        
+    while (number <= -1000 || number >= 1000)
     {
         number = number / 10;    
     } 
-    while (number <= -1000)
-    {
-        number = number / 10;    
-    } 
-    if (number > 99)
+    if (number < -99 || number >99)
     {
         number = number % 10;
+    }
+    else
+    {
+        Console.WriteLine ($"{number} There is no third number"); 
     }
         
-    if (number < -99)
-    {
-        number = number % 10;
-    }
-    
-        else
-            Console.WriteLine ("Here is no third digit"); 
-return number;
+    return number;
 }
-
+   
  Console.Write ("Enter a number: ");
  int number = Convert.ToInt32(Console.ReadLine());
 
  int ThreeDigit = MethodThirdNumber(number);
 
 Console.WriteLine ($"The third number from {number} this {ThreeDigit}");
+*/
 
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день 
+// выходным.
+
+int TheDaysOfTheWeekMethod(int dayNumber);
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine ($"It's a day off");
+    }
+    else
+    {
+        Console.WriteLine ($"This day is not a day off")
+    }
+    return dayNumber;
+}
+
+Console.Write ("Enter a number indicating the day of the week: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+ int IndicatingNumber = TheDaysOfTheWeekMethod(dayNumber);
+Console.WriteLine ($"This number indicates the day of the week, which is {IndicatingNumber}");
