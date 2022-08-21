70eb4ea233b09911415b82a6262986c365f2c23a
Arnold <usov.a38@mail.ru>
2022-08-21 18:41:40 +0800

    SecondSeminarCommit11

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*
+// Задача №25. Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+int Exponentiation (int numberA, int numberB)
+{
+    int result = Convert.ToInt32(Math.Pow(numberA, numberB));
+    return result;
+}
+
+Console.Write("Enter the number A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int Inkind = Exponentiation(numberA, numberB);
+Console.WriteLine($" = {Inkind}");
+*/
+
+/*
+// Задача №27. Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+int SumNumber(int numberN)
+{
+    int counter=Convert.ToString(numberN).Length;
+    int advance=0;
+    int result=0;
+    for(int i=0; i<counter; i++)
+    {
+        advance=numberN-numberN % 10;
+        result=result + (numberN-advance);
+        numberN=numberN / 10;
+    }
+    return result;
+}
+Console.Write("Enter number N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ($"The sum of the numbers in the number N: {SumNumber(numberN)}");
+*/
+/*
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+*/
+
+            Я НЕ СПРАВИЛСЯ Т.Е. НЕ ЧЕГО НЕПОНЯЛ.
+            
+/*
+int[] CreatArray (int[] size)
+{
+    int[] newArray = new int[size];
+    Console.Write("Enter array element №1: ");
+    int first = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter array element №2: ");
+    int second = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter array element №3: ");
+    int third = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter array element №4: ");
+    int fourth = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter array element №5: ");
+    int fifth = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter array element №6: ");
+    int sixth = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter array element №7: ");
+    int seventh = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter array element №8: ");
+    int eight = Convert.ToInt32(Console.ReadLine());
+
+    return newArray;
+}
+Console.Write("Input size for array ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] MyArray=CreatArray(a);
+Console.WriteLine($"что то это {MyArray}");
+*/
